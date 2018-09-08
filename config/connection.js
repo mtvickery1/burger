9b5070a4940b538374c233f58f61c2f238540b87
mv04 <mtvickery1@gmail.com>
2018-09-07 22:36:17 -0500

    incorporated JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: process.env.DB_HOST || "127.0.0.1",
-    port: parseInt(process.env.DB_PORT) || 8889,
-    user: process.env.DB_USERNAME || "root",
-    password: process.env.DB_PASSWORD || "root",
-    database: "burgers_db"
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    var connection = mysql.createConnection(process.env.JAWSDB_URL);
+  } else {
+    var connection = mysql.createConnection({
+      host: "127.0.0.1",
+      user: "root",
+      password: "root",
+      database: "burgers_db",
+      port: 8889
+    });
+  };
 
 connection.connect(function(err) {
     if (err) {
